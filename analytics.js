50ea9486668b4c009c28337bf05069bf008cade9
Andrew Bialecki <andrew.bialecki@gmail.com>
2012-12-12 18:19:07 -0500

    Adding in Klaviyo provider and tests

--- analytics.js
+++ analytics.js
@@ -577,6 +577,44 @@
             }
 
             // TODO: Add virtual page API.
+        },
+
+        // Klaviyo
+        // --------
+        // _Last updated: December 12th, 2012_
+        //
+        // [Documentation](https://www.klaviyo.com/docs).
+        // [Documentation](https://www.klaviyo.com/docs/http-api).
+
+        'Klaviyo' : {
+
+            initialize : function (settings) {
+                this.settings = settings = resolveSettings(settings, 'apiKey');
+
+                var _learnq = _learnq || [];
+
+                _learnq.push(['account', settings.apiKey]);
+                
+                (function () {
+                    var b = document.createElement('script'); b.type = 'text/javascript'; b.async = true;
+                    b.src = ('https:' == document.location.protocol ? 'https://' : 'http://') +
+                        'a.klaviyo.com/media/js/learnmarklet.js';
+                    var a = document.getElementsByTagName('script')[0]; a.parentNode.insertBefore(b, a);
+                })();
+
+                window._learnq = _learnq;
+            },
+
+            identify : function (userId, traits) {
+                traits = traits || {};
+                if (userId)
+                  traits['$id'] = userId;
+                window._learnq.push(['identify', traits]);
+            },
+
+            track : function (event, properties) {
+                window._learnq.push(['track', event, properties]);
+            }
         }
     };
 

63a886bcff6a789cf0b1d08aef34097760878491
Peter Reinhardt <peter@segment.io>
2012-12-20 11:06:20 -0800

    fixing iterator for geturl parameter

--- src/analytics.js
+++ src/analytics.js
@@ -268,8 +268,8 @@
             // A helper to track events based on the 'anjs' url parameter
             getUrlParameter : function (urlSearchParameter, paramKey) {
                 var params = urlSearchParameter.replace('?', '').split('&');
-                for (var key in params) {
-                    var param = params[key].split('=');
+                for (var i = 0; i < params.length; i += 1) {
+                    var param = params[i].split('=');
                     if (param.length === 2 && param[0] === paramKey) {
                         return decodeURIComponent(param[1]);
                     }

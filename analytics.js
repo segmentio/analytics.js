f092d3446dd245e1f7cda10c32a42af8a9ac4d08
Peter Reinhardt <peter@segment.io>
2013-01-03 19:05:52 -0800

    adding support for errorception with a basic test like for chartbeat

--- analytics.js
+++ analytics.js
@@ -457,6 +457,61 @@ analytics.addProvider('Customer.io', {
 });
 
 
+// Errorception
+// ------------
+// [Documentation](http://errorception.com/).
+
+analytics.addProvider('Errorception', {
+
+    settings : {
+        projectId : null
+    },
+
+
+    // Initialize
+    // ----------
+
+    initialize : function (settings) {
+        settings = analytics.utils.resolveSettings(settings, 'projectId');
+        analytics.utils.extend(this.settings, settings);
+
+        var self = this;
+
+        var _errs = [settings.projectId];
+        (function(a,b){
+            a.onerror = function () {
+                _errs.push(arguments);
+            };
+            var d = function () {
+                var a = b.createElement("script"),
+                    c = b.getElementsByTagName("script")[0];
+                a.src = "//d15qhc0lu1ghnk.cloudfront.net/beacon.js";
+                a.async = true;
+                c.parentNode.insertBefore(a,c);
+            };
+            a.addEventListener ? a.addEventListener("load",d,!1) : a.attachEvent("onload",d);
+        })(window,document);
+    },
+
+
+    // Identify
+    // --------
+
+    identify : function (userId, traits) {
+        // noop
+    },
+
+
+    // Track
+    // -----
+
+    track : function (event, properties) {
+        // noop
+    }
+
+});
+
+
 // Google Analytics
 // ----------------
 // [Documentation](https://developers.google.com/analytics/devguides/collection/gajs/).

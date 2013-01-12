182a394c8f488b02290ca50029426936a026e728
Calvin French-Owen <calvin@segment.io>
2013-01-11 19:49:47 -0800

    Updating clicky test to run more quickly

--- src/providers/clicky/clicky-test.js
+++ src/providers/clicky/clicky-test.js
@@ -8,18 +8,31 @@
 
     test('stores settings and adds clicky.js on initialize', function (callback) {
 
-        expect(window.clicky).not.to.exist;
-
-        // Clicky loads slowly on travis
+        // Clicky loads very slowly on travis, so we
+        // have to use a slightly different structure.
         this.timeout(8000);
+        var loadedCheck;
+
+        function onFinish() {
+            expect(window.clicky).to.exist;
+            clearInterval(loadedCheck);
+            callback();
+        }
+
+        expect(window.clicky).not.to.exist;
 
         analytics.initialize({
             'Clicky' : 'x'
         });
-        setTimeout(function () {
-            expect(window.clicky).to.exist;
-            callback();
-        }, 7000);
+
+        // Check to see whether clicky has loaded.
+        // Avoids long timeout when testing locally.
+        loadedCheck = setInterval(function () {
+            if (window.clicky)
+              onFinish();
+        }, 500);
+
+        setTimeout(onFinish, 7000);
         expect(analytics.providers[0].settings.siteId).to.equal('x');
     });
 

4c2e579d42cc5c0e64c2c000a3a29d7a42045c69
Peter Reinhardt <peter@segment.io>
2013-01-11 18:32:01 -0800

    updating minified test and adding comscore doc

--- analytics.js
+++ analytics.js
@@ -380,7 +380,40 @@ analytics.addProvider('Clicky', {
 });
 
 
-// CrazyEgg
+// Comscore
+// ---------
+// [Documentation](http://direct.comscore.com/clients/help/FAQ.aspx#faqTagging)
+
+analytics.addProvider('Comscore', {
+
+    settings : {
+        c1 : '2',
+        c2 : null
+    },
+
+
+    // Initialize
+    // ----------
+
+    initialize : function (settings) {
+        settings = analytics.utils.resolveSettings(settings, 'c2');
+        analytics.utils.extend(this.settings, settings);
+
+        var _comscore = window._comscore = _comscore || [];
+        _comscore.push(settings);
+        (function() {
+            var s = document.createElement("script");
+            var el = document.getElementsByTagName("script")[0];
+            s.async = true;
+            s.src = (document.location.protocol == "https:" ? "https://sb" : "http://b") + ".scorecardresearch.com/beacon.js";
+            el.parentNode.insertBefore(s, el);
+        })();
+
+        // NOTE: the <noscript><img> bit in the docs is ignored
+        // because we have to run JS in order to do any of this!
+    }
+
+});// CrazyEgg
 // --------
 // [Documentation](www.crazyegg.com).
 

b5c3310c7ae193c5dad533ed2f953b0a92a6df12
Daniel Kador <dkador@gmail.com>
2013-01-11 18:35:19 -0800

    Add support for Keen IO

--- analytics.js
+++ analytics.js
@@ -1184,7 +1184,7 @@ analytics.addProvider('Keen', {
         var Keen=Keen||{configure:function(a,b,c){this._pId=a;this._ak=b;this._op=c},addEvent:function(a,b,c,d){this._eq=this._eq||[];this._eq.push([a,b,c,d])},setGlobalProperties:function(a){this._gp=a},onChartsReady:function(a){this._ocrq=this._ocrq||[];this._ocrq.push(a)}};
         (function(){var a=document.createElement("script");a.type="text/javascript";a.async=!0;a.src=("https:"==document.location.protocol?"https://":"http://")+"cloudfront.keen.io/code/keen-2.0.0-min.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})();
         
-        // Configure the Keen object with your Project ID and API Key
+        // Configure the Keen object with your Project ID and API Key.
         Keen.configure(settings.projectId, settings.apiKey);
         
         window.Keen = Keen;
@@ -1194,7 +1194,7 @@ analytics.addProvider('Keen', {
     // Identify
     // --------
     identify: function(userId, traits) {
-        // use Keen IO global properties to include user ID and traits on every event sent to Keen IO
+        // Use Keen IO global properties to include user ID and traits on every event sent to Keen IO.
         var globalUserProps = {};
         if (userId) globalUserProps["userId"] = userId;
         if (traits) globalUserProps["traits"] = traits;
@@ -1211,7 +1211,7 @@ analytics.addProvider('Keen', {
     // Track
     // -----
     track: function(event, properties) {
-        // each track invocation will add a single event to Keen
+        // Each track invocation will add a single event to Keen.
         window.Keen.addEvent(event, properties);
     }
 

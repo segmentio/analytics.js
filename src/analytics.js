6407953b5872eac7c105f78fd4db26ff5caf8de6
Calvin French-Owen <calvin@segment.io>
2013-02-06 17:14:54 -0800

    Adding fix for IE pathname

--- src/analytics.js
+++ src/analytics.js
@@ -507,7 +507,7 @@
                     port     : a.port || location.port,
                     hash     : a.hash,
                     hostname : a.hostname || location.hostname,
-                    pathname : a.pathname,
+                    pathname : a.pathname.charAt(0) !== '/' ? '/' + a.pathname : a.pathname,
                     protocol : !a.protocol || ':' === a.protocol ? location.protocol : a.protocol,
                     search   : a.search,
                     query    : a.search.slice(1)

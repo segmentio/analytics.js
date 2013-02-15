8c5ff1678411c17820c0047d51aec5d683d80d09
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-02-14 17:50:35 -0800

    converting bitdeli to stndard test syntax

--- test/providers/bitdeli.js
+++ test/providers/bitdeli.js
@@ -1,148 +1,132 @@
-var analytics = require('analytics'),
-  clone     = require('component-clone');
-
-!(function (undefined) {
-
-  suite('Bitdeli');
-
-  var options = {
-    'Bitdeli' : {
-      inputId   : 'x',
-      authToken : 'y'
-    }
-  };
-
-  var event = 'event';
-
-  var properties = {
-    count : 42
-  };
-
-  var userId = 'user';
-
-  var traits = {
-    name  : 'Zeus',
-    email : 'zeus@segment.io'
-  };
-
-  var url = '/url';
+var analytics = require('analytics')
+  , clone     = require('component-clone')
+  , utils     = require('../utils');
 
 
-  // Initialize
-  // ----------
+describe('Bitdeli', function () {
 
-  test('calls ready and loads library on initialize', function (done) {
-    expect(window._bdq).to.be(undefined);
 
-    // The Bitdeli provider uses a queue, so it's ready right away.
-    var spy = sinon.spy();
-    analytics.ready(spy);
-    analytics.initialize(options);
-    expect(spy.called).to.be(true);
+  describe('initialize', function () {
 
-    // After initialize, the queue should be made, but the library isn't
-    // loaded yet, so `_version` isn't defined.
-    expect(window._bdq).not.to.be(undefined);
-    expect(window._bdq._version).to.be(undefined);
+    it('should call ready and load library', function (done) {
+      expect(window._bdq).to.be(undefined);
 
-    // Once the library loads, `_version` is defined.
-    setTimeout(function () {
-      expect(window._bdq._version).not.to.be(undefined);
-      done();
-    }, 1900);
-  });
+      // The Bitdeli provider uses a queue, so it's ready right away.
+      var spy = sinon.spy();
+      analytics.ready(spy);
+      analytics.initialize(utils['Bitdeli']);
+      expect(spy.called).to.be(true);
 
-  test('stores options on initialize', function () {
-    analytics.initialize(options);
-    expect(analytics.providers[0].options.inputId).to.equal('x');
-    expect(analytics.providers[0].options.authToken).to.equal('y');
-  });
+      // After initialize, the queue should be made, but the library isn't
+      // loaded yet, so `_version` isn't defined.
+      expect(window._bdq).not.to.be(undefined);
+      expect(window._bdq._version).to.be(undefined);
 
-  test('pushes "trackPageview" on initialize', function () {
-    window._bdq = [];
-    var spy = sinon.spy(window._bdq, 'push');
+      // Once the library loads, `_version` is defined.
+      setTimeout(function () {
+        expect(window._bdq._version).not.to.be(undefined);
+        done();
+      }, 1900);
+    });
 
-    analytics.initialize({ 'Bitdeli' : options });
+    it('should store options', function () {
+      analytics.initialize(utils['Bitdeli']);
+      var options = analytics.providers[0].options;
+      expect(options.inputId).to.equal(utils['Bitdeli'].inputId);
+      expect(options.authToken).to.equal(utils['Bitdeli'].authToken);
+    });
 
-    expect(spy.calledWith(['trackPageview', undefined])).to.be(true);
-    spy.restore();
-  });
+    it('should track a pageview', function () {
+      // Create the queue before hand, so we can spy it.
+      window._bdq = [];
+      var spy = sinon.spy(window._bdq, 'push');
 
-  test('can disable initial "trackPageview" on initialize', function () {
-    window._bdq = [];
-    var spy = sinon.spy(window._bdq, 'push');
+      analytics.initialize(utils['Bitdeli']);
+      expect(spy.calledWith(['trackPageview', undefined])).to.be(true);
 
-    var modifiedSettings = clone(options);
-    modifiedSettings.initialPageview = false;
-    analytics.initialize({ 'Bitdeli' : modifiedSettings });
+      spy.restore();
+    });
 
-    expect(spy.calledWith(['trackPageview'])).to.be(false);
-    spy.restore();
+    it('should be able to not track a pageview', function () {
+      // Create the queue before hand, so we can spy it.
+      window._bdq = [];
+      var spy = sinon.spy(window._bdq, 'push');
+      var options = clone(utils['Bitdeli']);
+      options.initialPageview = false;
+
+      analytics.initialize({ 'Bitdeli' : options });
+      expect(spy.calledWith(['trackPageview'])).to.be(false);
+
+      spy.restore();
+    });
+
   });
 
 
-  // Identify
-  // --------
+  describe('identify', function () {
 
-  test('pushes "identify" on identify', function () {
-    var spy = sinon.spy(window._bdq, 'push');
-    analytics.identify(traits);
-    expect(spy.calledWith(['identify', userId])).to.be(false);
+    it('should push "identify" on identify', function () {
+      var spy = sinon.spy(window._bdq, 'push');
+      analytics.identify(utils.traits);
+      expect(spy.calledWith(['identify', utils.userId])).to.be(false);
 
-    spy.reset();
-    analytics.identify(userId);
-    expect(spy.calledWith(['identify', userId])).to.be(true);
+      spy.reset();
+      analytics.identify(utils.userId);
+      expect(spy.calledWith(['identify', utils.userId])).to.be(true);
 
-    spy.reset();
-    analytics.identify(userId, traits);
-    expect(spy.calledWith(['identify', userId])).to.be(true);
+      spy.reset();
+      analytics.identify(utils.userId, utils.traits);
+      expect(spy.calledWith(['identify', utils.userId])).to.be(true);
 
-    spy.restore();
-  });
+      spy.restore();
+    });
 
-  test('pushes "set" on identify', function () {
-    var spy = sinon.spy(window._bdq, 'push');
-    analytics.identify(traits);
-    expect(spy.calledWith(['set', traits])).to.be(true);
+    it('should push "set" on identify', function () {
+      var spy = sinon.spy(window._bdq, 'push');
+      analytics.identify(utils.traits);
+      expect(spy.calledWith(['set', utils.traits])).to.be(true);
 
-    spy.reset();
-    analytics.identify(userId);
-    expect(spy.calledWith(['set', traits])).to.be(false);
+      spy.reset();
+      analytics.identify(utils.userId);
+      expect(spy.calledWith(['set', utils.traits])).to.be(false);
 
-    spy.reset();
-    analytics.identify(userId, traits);
-    expect(spy.calledWith(['set', traits])).to.be(true);
+      spy.reset();
+      analytics.identify(utils.userId, utils.traits);
+      expect(spy.calledWith(['set', utils.traits])).to.be(true);
 
-    spy.restore();
-  });
+      spy.restore();
+    });
 
-
-  // Track
-  // -----
-
-  test('pushes "track" on track', function () {
-    var spy = sinon.spy(window._bdq, 'push');
-    analytics.track(event, properties);
-    expect(spy.calledWith(['track', event, properties])).to.be(true);
-
-    spy.restore();
   });
 
 
-  // Pageview
-  // --------
+  describe('track', function () {
 
-  test('pushes "trackPageview" on pageview', function () {
-    var spy = sinon.spy(window._bdq, 'push');
-    analytics.pageview();
-    expect(spy.calledWith(['trackPageview', undefined])).to.be(true);
+    it('should push "track" on track', function () {
+      var spy = sinon.spy(window._bdq, 'push');
+      analytics.track(utils.event, utils.properties);
+      expect(spy.calledWith(['track', utils.event, utils.properties])).to.be(true);
 
-    spy.reset();
-    analytics.pageview(url);
-    expect(spy.calledWith(['trackPageview', url])).to.be(true);
+      spy.restore();
+    });
 
-    spy.restore();
   });
 
 
-}());
+  describe('pageview', function () {
+
+    it('should push "trackPageview" on pageview', function () {
+      var spy = sinon.spy(window._bdq, 'push');
+      analytics.pageview();
+      expect(spy.calledWith(['trackPageview', undefined])).to.be(true);
+
+      spy.reset();
+      analytics.pageview(utils.url);
+      expect(spy.calledWith(['trackPageview', utils.url])).to.be(true);
+
+      spy.restore();
+    });
+
+  });
+
+});

2788afdd0fcd4bb75db4224109757d4a360b0534
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-03-06 14:42:26 -0800

    add extra mixpanel methods to snippet, fixes #105

--- analytics.js
+++ analytics.js
@@ -3383,7 +3383,7 @@ module.exports = Provider.extend({
         var g = a;
         'undefined' !== typeof f ? g = a[f] = [] : f = 'mixpanel';
         g.people = g.people || [];
-        h = ['disable', 'track', 'track_pageview', 'track_links', 'track_forms', 'register', 'register_once', 'unregister', 'identify', 'alias', 'name_tag', 'set_config', 'people.set', 'people.increment'];
+        h = ['disable', 'track', 'track_pageview', 'track_links', 'track_forms', 'register', 'register_once', 'unregister', 'identify', 'alias', 'name_tag', 'set_config', 'people.set', 'people.increment', 'people.track_charge', 'people.append'];
         for (e = 0; e < h.length; e++) d(g, h[e]);
         a._i.push([b, c, f]);
       };

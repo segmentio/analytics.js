8ceb92b9b43d5b546f27fe28b7b405e637675511
Peter Reinhardt <peter@segment.io>
2013-03-06 17:58:03 -0800

    updating tests and rebuilding

--- test/providers/clicktale.js
+++ test/providers/clicktale.js
@@ -25,6 +25,10 @@ describe('ClickTale', function () {
     it('should store options', function () {
       analytics.initialize({ 'ClickTale' : test['ClickTale'] });
       expect(analytics.providers[0].options.projectId).to.equal('19370');
+      expect(analytics.providers[0].options.recordingRatio).to.equal('0.0089');
+      expect(analytics.providers[0].options.partitionId).to.equal('www14');
+      expect(analytics.providers[0].options.normalCDNUrl).to.not.equal(null);
+      expect(analytics.providers[0].options.secureCDNUrl).to.equal(null);
     });
 
   });

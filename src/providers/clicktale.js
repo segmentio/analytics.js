d379a1f4ecfbd91c35c35915bbb335020efa1391
Peter Reinhardt <peter@segment.io>
2013-03-06 17:55:31 -0800

    adding comments to settings and functions

--- src/providers/clicktale.js
+++ src/providers/clicktale.js
@@ -12,17 +12,32 @@ module.exports = Provider.extend({
   key : 'projectId',
 
   options : {
+
+    // There's a default insecure CDN url that most projects seem to use.
     normalCDNUrl   : 'http://s.clicktale.net/WRe0.js',
+
+    // SSL support is only for premium accounts.
     secureCDNUrl   : null,
 
+    // The Project ID is loaded in after the ClickTale CDN javascript has loaded.
     projectId      : null,
+
+    // The recording ratio specifies what fraction of people to screen-record.
+    // ClickTale has a special calculator in their setup flow that tells you
+    // what number to set for this.
     recordingRatio : 0.01,
+
+    // The Partition ID determines where ClickTale stores the data according to 
+    // http://wiki.clicktale.com/Article/JavaScript_API
     partitionId    : null
   },
 
 
   initialize : function (options, ready) {
 
+    // ClickTale wants this at the "top" of the page. The
+    // analytics.js snippet sets this date synchronously now,
+    // and  makes it available via load-date.
     window.WRInitTime = date.getTime();
 
     var onloaded = function () {
@@ -32,7 +47,7 @@ module.exports = Provider.extend({
 
     // Load the appropriate CDN library, if no
     // ssl library is provided and we're on ssl then
-    // we can't load anything... (for non-premium accounts.)
+    // we can't load anything (always true for non-premium accounts.)
     if (document.location.protocol !== 'https:')
       load(options.normalCDNUrl, onloaded);
     else if (options.secureCDNUrl)
@@ -40,8 +55,10 @@ module.exports = Provider.extend({
   },
 
   identify : function (userId, traits) {
+    // We set the userId as the ClickTale UID.
     if (window.ClickTaleSetUID) window.ClickTaleSetUID(userId);
 
+    // We iterate over all the traits and set them as key-value field pairs.
     if (window.ClickTaleField) {
       for (var traitKey in traits) {
         window.ClickTaleField(traitKey, traits[traitKey]);
@@ -50,6 +67,7 @@ module.exports = Provider.extend({
   },
 
   track : function (event, properties) {
+    // ClickTaleEvent is an alias for ClickTaleTag
     if (window.ClickTaleEvent) window.ClickTaleEvent(event);
   }
 

74c0631747912a34fb14113754eddf2c434fd23b
Aaron Raddon <araddon@gmail.com>
2013-03-18 12:20:47 -0700

    Lytics initial implementation

--- src/providers/index.js
+++ src/providers/index.js
@@ -19,6 +19,7 @@ module.exports = [
   require('./kissmetrics'),
   require('./klaviyo'),
   require('./livechat'),
+  require('./lytics'),
   require('./mixpanel'),
   require('./olark'),
   require('./perfect-audience'),

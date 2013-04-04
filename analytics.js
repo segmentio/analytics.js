eedea378f448e0b9d3fc250009216dd21db22c49
Calvin French-Owen <calvin@segment.io>
2013-04-04 15:14:51 -0700

    Updating

--- analytics.js
+++ analytics.js
@@ -3479,8 +3479,8 @@ var Provider = require('../provider')
 
 module.exports = Provider.extend({
 
-    name : 'Lytics', 
-    
+    name : 'Lytics',
+
     key : 'cid',
 
     options : {
@@ -3494,14 +3494,14 @@ module.exports = Provider.extend({
           t.send=function(){
             this._q.push(["ready","send",Array.prototype.slice.call(arguments)]);
             return this;
-          }
-          return t
+          };
+          return t;
         })();
 
         load('//c.lytics.io/static/io.min.js');
 
-        // ready immediately 
-        ready()
+        // ready immediately
+        ready();
     },
 
 

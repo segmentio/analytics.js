b24703348dbad8ac61f4321466d976c1837c70d3
Peter Reinhardt <peter@segment.io>
2013-04-26 18:35:37 -0700

    updating mocha to fix a weird testing issue im seeing, and fixing uservoice tests and aliasing

--- analytics.js
+++ analytics.js
@@ -4217,7 +4217,8 @@ require.register("analytics/src/providers/uservoice.js", function(exports, requi
 // http://feedback.uservoice.com/knowledgebase/articles/225-how-do-i-pass-custom-data-through-the-widget-and-i
 
 var Provider = require('../provider')
-  , load     = require('load-script');
+  , load     = require('load-script')
+  , alias    = require('alias');
 
 
 module.exports = Provider.extend({
@@ -4227,35 +4228,49 @@ module.exports = Provider.extend({
   defaults : {
     // These first two options are required.
     widgetId          : null,
-    forum_id          : null,
+    forumId           : null,
     // Should we show the tab automatically?
     showTab           : true,
     // There's tons of options for the tab.
     mode              : 'full',
-    primary_color     : '#cc6d00',
-    link_color        : '#007dbf',
-    default_mode      : 'support',
-    support_tab_name  : null,
-    feedback_tab_name : null,
-    tab_label         : 'Feedback & Support',
-    tab_color         : '#cc6d00',
-    tab_position      : 'middle-right',
-    tab_inverted      : false
+    primaryColor      : '#cc6d00',
+    linkColor         : '#007dbf',
+    defaultMode       : 'support',
+    supportTabName    : null,
+    feedbackTabName   : null,
+    tabLabel          : 'Feedback & Support',
+    tabColor          : '#cc6d00',
+    tabPosition       : 'middle-right',
+    tabInverted       : false
   },
 
   initialize : function (options, ready) {
     window.UserVoice = window.UserVoice || [];
     load('//widget.uservoice.com/' + options.widgetId + '.js', ready);
 
+    alias(options, {
+      'forumId'         : 'forum_id',
+      'primaryColor'    : 'primary_color',
+      'linkColor'       : 'link_color',
+      'defaultMode'     : 'default_mode',
+      'supportTabName'  : 'support_tab_name',
+      'feedbackTabName' : 'feedback_tab_name',
+      'tabLabel'        : 'tab_label',
+      'tabColor'        : 'tab_color',
+      'tabPosition'     : 'tab_position',
+      'tabInverted'     : 'tab_inverted'
+    });
+
     // If we don't automatically show the tab, let them show it via 
     // javascript. This is the default name for the function in their snippet.
     window.showClassicWidget = function (showWhat) {
-      UserVoice.push([showWhat || 'showLightbox', 'classic_widget', options]);
+      window.UserVoice.push([showWhat || 'showLightbox', 'classic_widget', options]);
     };
 
     // If we *do* automatically show the tab, get on with it!
-    if (options.showTab)
+    if (options.showTab) {
       window.showClassicWidget('showTab');
+    }
   }
 
 });
--- test/providers/uservoice.js
+++ test/providers/uservoice.js
@@ -14,6 +14,23 @@ describe('UserVoice', function () {
       expect(window.UserVoice).not.to.be(undefined);
       expect(window.UserVoice.push).to.be(Array.prototype.push);
 
+      // The proper options should get loaded.
+      expect(window.UserVoice[0]).to.eql(['showTab', 'classic_widget', {
+        widgetId          : test['UserVoice'].widgetId,
+        showTab           : true,
+        forum_id          : test['UserVoice'].forumId,
+        mode              : 'full',
+        primary_color     : '#cc6d00',
+        link_color        : '#007dbf',
+        default_mode      : 'support',
+        support_tab_name  : null,
+        feedback_tab_name : null,
+        tab_label         : 'Feedback & Support',
+        tab_color         : '#cc6d00',
+        tab_position      : 'middle-right',
+        tab_inverted      : false
+      }]);
+
       // Once the library loads, `_uvts` gets set.
       var interval = setInterval(function () {
         if (window.UserVoice.push === Array.prototype.push) return;
@@ -27,7 +44,6 @@ describe('UserVoice', function () {
     it('should store options', function () {
       analytics.initialize({ 'UserVoice' : test['UserVoice'] });
       expect(analytics.providers[0].options.widgetId).to.equal(test['UserVoice'].widgetId);
-      expect(analytics.providers[0].options.forumId).to.equal(test['UserVoice'].forumId);
       expect(analytics.providers[0].options.showTab).to.equal(true);
     });
 

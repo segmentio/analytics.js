12fc32722530ce7f1b9a60c5b2bb27d73e3353bc
Peter Reinhardt <peter@segment.io>
2013-04-26 18:01:38 -0700

    adding bugherd option to hide tab

--- analytics.js
+++ analytics.js
@@ -2452,10 +2452,16 @@ module.exports = Provider.extend({
   key : 'apiKey',
 
   defaults : {
-    apiKey : null
+    apiKey : null,
+    // Optionally hide the feedback tab if you want to build your own.
+    // http://support.bugherd.com/entries/21497629-Create-your-own-Send-Feedback-tab
+    showFeedbackTab : true
   },
 
   initialize : function (options, ready) {
+    if (!options.showFeedbackTab) {
+        window.BugHerdConfig = { "feedback" : { "hide" : true } };
+    }
     load('//www.bugherd.com/sidebarv2.js?apikey=' + options.apiKey, ready);
   }
 

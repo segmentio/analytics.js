48946877d561f2f518282df2d83387fab7712f0f
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-04 13:53:08 -0700

    cleaned bugherd integration

--- test/providers/bugherd.js
+++ test/providers/bugherd.js
@@ -1,38 +1,56 @@
+
 describe('BugHerd', function () {
 
-  var analytics = require('analytics');
+var analytics = window.analytics || require('analytics')
+  , assert = require('assert')
+  , equal = require('equals')
+  , sinon = require('sinon')
+  , when = require('when');
 
+var settings = {
+  apiKey: '7917d741-16cc-4c2b-bb1a-bdd903d53d72',
+  showFeedbackTab: false
+};
 
-  describe('initialize', function () {
+before(function (done) {
+  this.timeout(10000);
+  this.spy = sinon.spy();
+  analytics.ready(this.spy);
+  analytics.initialize({ BugHerd: settings });
+  this.integration = analytics._integrations.BugHerd;
+  this.options = this.integration.options;
+  when(function () { return window._bugHerd; }, done);
+});
 
-    this.timeout(10000);
-
-    it('should call ready and load library', function (done) {
-      expect(window._bugHerd).to.be(undefined);
-
-      var spy = sinon.spy();
-      analytics.ready(spy);
-      analytics.initialize({ 'BugHerd' : test['BugHerd'] });
-
-      // Once the library loads, `_bugHerd` gets set.
-      var interval = setInterval(function () {
-        if (!window._bugHerd) return;
-        expect(window._bugHerd).not.to.be(undefined);
-        expect(spy.called).to.be(true);
-        clearInterval(interval);
-        done();
-      }, 20);
-    });
-
-    it('should store options', function () {
-      analytics.initialize({ 'BugHerd' : test['BugHerd'] });
-      expect(analytics._providers[0].options.apiKey).to.equal(test['BugHerd']);
-    });
-
-    it('should not show tab if set', function () {
-      analytics.initialize({ 'BugHerd' : { apiKey : test['BugHerd'], showFeedbackTab : false } });
-    });
-
+describe('#key', function () {
+  it('apiKey', function () {
+    assert(this.integration.key == 'apiKey');
   });
+});
+
+describe('#defaults', function () {
+  it('apiKey', function () {
+    assert(this.integration.defaults.apiKey === '');
+  });
+
+  it('showFeedbackTab', function () {
+    assert(this.integration.defaults.showFeedbackTab === true);
+  });
+});
+
+describe('#initialize', function () {
+  it('should call ready', function () {
+    assert(this.spy.called);
+  });
+
+  it('should store options with defaults', function () {
+    assert(this.options.apiKey == settings.apiKey);
+    assert(this.options.showFeedbackTab == settings.showFeedbackTab);
+  });
+
+  it('should hide feedback tab', function () {
+    assert(equal(window.BugHerdConfig, { feedback: { hide: true }}));
+  });
+});
 
 });

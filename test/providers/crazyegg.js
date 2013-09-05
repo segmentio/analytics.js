44f504f96e469dd01b5b85927e5883d9bc430ae5
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-04 17:18:31 -0700

    cleaned up crazyegg integration

--- test/providers/crazyegg.js
+++ test/providers/crazyegg.js
@@ -1,35 +1,46 @@
+
 describe('CrazyEgg', function () {
 
-  var analytics = require('analytics');
+var analytics = window.analytics || require('analytics')
+  , assert = require('assert')
+  , equal = require('equals')
+  , sinon = require('sinon')
+  , when = require('when');
 
+var settings = {
+  accountNumber: '00138301'
+};
 
-  describe('initialize', function () {
+before(function (done) {
+  this.timeout(10000);
+  this.spy = sinon.spy();
+  analytics.ready(this.spy);
+  analytics.initialize({ CrazyEgg: settings });
+  this.integration = analytics._integrations.CrazyEgg;
+  this.options = this.integration.options;
+  when(function () { return window.CE2; }, done);
+});
 
-    this.timeout(10000);
-
-    it('should call ready and load library', function (done) {
-      expect(window.CE2).to.be(undefined);
-
-      var spy = sinon.spy();
-      analytics.ready(spy);
-      analytics.initialize({ 'CrazyEgg' : test['CrazyEgg'] });
-
-      // When the library loads, `CE2` is created.
-      // var interval = setInterval(function () {
-      //   if (!window.CE2) return;
-      //   expect(window.CE2).not.to.be(undefined);
-      //   expect(spy.called).to.be(true);
-      //   clearInterval(interval);
-      //   done();
-      // }, 20);
-      done();
-    });
-
-    it('should store options', function () {
-      analytics.initialize({ 'CrazyEgg' : test['CrazyEgg'] });
-      expect(analytics._providers[0].options.accountNumber).to.equal('00138301');
-    });
-
+describe('#key', function () {
+  it('accountNumber', function () {
+    assert(this.integration.key == 'accountNumber');
   });
+});
+
+describe('#defaults', function () {
+  it('accountNumber', function () {
+    assert(this.integration.defaults.accountNumber === '');
+  });
+});
+
+describe('#initialize', function () {
+  it('should call ready', function () {
+    assert(this.spy.called);
+  });
+
+  it('should store options', function () {
+    assert(this.options.accountNumber == settings.accountNumber);
+  });
+});
 
 });

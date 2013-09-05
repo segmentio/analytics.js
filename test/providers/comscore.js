f71997473cb611450596f3cb0cf5a61ae77ca5c9
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-04 17:10:32 -0700

    cleaned up comscore integration

--- test/providers/comscore.js
+++ test/providers/comscore.js
@@ -1,38 +1,51 @@
+
 describe('comScore', function () {
 
+var analytics = window.analytics || require('analytics')
+  , assert = require('assert')
+  , equal = require('equals')
+  , sinon = require('sinon')
+  , when = require('when');
 
-  var analytics = require('analytics');
+var settings = {
+  c2: 'x'
+};
 
-  describe('initialize', function () {
+before(function (done) {
+  this.timeout(10000);
+  this.spy = sinon.spy();
+  analytics.ready(this.spy);
+  analytics.initialize({ comScore: settings });
+  this.integration = analytics._integrations.comScore;
+  this.options = this.integration.options;
+  when(function () { return window.COMSCORE; }, done);
+});
 
-    this.timeout(10000);
-
-    it('should call ready and load library and store options', function (done) {
-      expect(window._comscore).to.be(undefined);
-
-      var spy = sinon.spy();
-      analytics.ready(spy);
-      analytics.initialize({ 'comScore' : test['comScore'] });
-      expect(window._comscore).not.to.be(undefined);
-
-      // Wait for the library to load for ready to be called.
-      var interval = setInterval(function () {
-        if (!spy.called) return;
-        expect(spy.called).to.be(true);
-        clearInterval(interval);
-        done();
-      }, 20);
-    });
-
-    // The `c1` variable is hardcoded and not passed in.
-    it('should store options', function () {
-      analytics.initialize({ 'comScore' : test['comScore'] });
-      expect(window._comscore[0].c1).to.equal('2');
-      expect(window._comscore[0].c2).to.equal(test['comScore'].c2);
-      expect(analytics._providers[0].options.c1).to.equal('2');
-      expect(analytics._providers[0].options.c2).to.equal(test['comScore'].c2);
-    });
-
+describe('#key', function () {
+  it('c2', function () {
+    assert(this.integration.key == 'c2');
   });
+});
+
+describe('#defaults', function () {
+  it('c1', function () {
+    assert(this.integration.defaults.c1 === '2');
+  });
+
+  it('c2', function () {
+    assert(this.integration.defaults.c2 === '');
+  });
+});
+
+describe('#initialize', function () {
+  it('should call ready', function () {
+    assert(this.spy.called);
+  });
+
+  it('should store options with defaults', function () {
+    assert(this.options.c2 == settings.c2);
+    assert(this.options.c1 == this.integration.defaults.c1);
+  });
+});
 
 });

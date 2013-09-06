0b61189e06c3d6b9480c0be542de462c8b4801a3
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-05 17:54:22 -0700

    heap: cleanup

--- test/providers/heap.js
+++ test/providers/heap.js
@@ -1,50 +1,92 @@
+
 describe('Heap', function () {
 
-  var analytics = require('analytics')
-    , tick = require('next-tick');
+var analytics = window.analytics || require('analytics')
+  , assert = require('assert')
+  , equal = require('equals')
+  , sinon = require('sinon')
+  , when = require('when');
 
-  describe('initialize', function () {
+var settings = {
+  apiKey: 'x'
+};
 
-    it('should call ready and load library', function () {
-      expect(window.heap).to.be(undefined);
+before(function (done) {
+  this.timeout(10000);
+  this.spy = sinon.spy();
+  analytics.ready(this.spy);
+  analytics.initialize({ Heap: settings });
+  this.integration = analytics._integrations.Heap;
+  this.options = this.integration.options;
+  var stub = window.heap;
+  when(function () { return window.heap != stub; }, done);
+});
 
-      var spy = sinon.spy();
-      analytics.ready(spy);
-      analytics.initialize({ 'Heap' : test['Heap'] });
-      expect(window.heap).not.to.be(undefined);
+describe('#key', function () {
+  it('apiKey', function () {
+    assert(this.integration.key == 'apiKey');
+  });
+});
 
-      tick(function () {
-        expect(spy.called).to.be(true);
-      });
-    });
+describe('#defaults', function () {
+  it('apiKey', function () {
+    assert(this.integration.defaults.apiKey === '');
+  });
+});
 
-    it('should store options', function () {
-      analytics.initialize({ 'Heap' : test['Heap'] });
-      expect(analytics._providers[0].options.apiKey).to.equal(test['Heap']);
-    });
-
+describe('#initialize', function () {
+  it('should call ready', function () {
+    assert(this.spy.called);
   });
 
-
-  describe('identify', function () {
-
-    it('should call heap.identify', function () {
-      var identifySpy = sinon.spy(window.heap, 'identify');
-      analytics.identify(test.traits);
-      expect(identifySpy.calledWith(test.traits)).to.be(true);
-      identifySpy.restore();
-    });
+  it('should store options', function () {
+    assert(this.options.apiKey == settings.apiKey);
   });
 
-
-  describe('track', function () {
-
-    it('should call heap.track', function () {
-      var trackSpy = sinon.spy(window.heap, 'track');
-      analytics.track(test.event, test.properties);
-      expect(trackSpy.calledWith(test.event, test.properties)).to.be(true);
-      trackSpy.restore();
-    });
+  it('should pass options to Heap', function () {
+    assert(window._heapid == settings.apiKey);
   });
+});
+
+describe('#identify', function () {
+  beforeEach(function () {
+    analytics._user.clear();
+    this.spy = sinon.spy(window.heap, 'identify');
+  });
+
+  afterEach(function () {
+    this.spy.restore();
+  });
+
+  it('should send traits', function () {
+    analytics.identify({ trait: true });
+    assert(this.spy.calledWith({ trait: true }));
+  });
+
+  it('should alias a username', function () {
+    analytics.identify({ username: 'username' });
+    assert(this.spy.calledWith({ handle: 'username' }));
+  });
+});
+
+describe('#track', function () {
+  beforeEach(function () {
+    this.spy = sinon.spy(window.heap, 'track');
+  });
+
+  afterEach(function () {
+    this.spy.restore();
+  });
+
+  it('should send an event', function () {
+    analytics.track('event');
+    assert(this.spy.calledWith('event'));
+  });
+
+  it('should send an event and properties', function () {
+    analytics.track('event', { property: true });
+    assert(this.spy.calledWith('event', { property: true }));
+  });
+});
 
 });

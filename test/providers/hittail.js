110603a14489265dc3ad17a014fc1817845c02d3
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-05 18:25:20 -0700

    hittail: cleanup

--- test/providers/hittail.js
+++ test/providers/hittail.js
@@ -1,34 +1,46 @@
+
 describe('HitTail', function () {
 
-  var analytics = require('analytics');
+var analytics = window.analytics || require('analytics')
+  , assert = require('assert')
+  , equal = require('equals')
+  , sinon = require('sinon')
+  , when = require('when');
 
+var settings = {
+  siteId: 'x'
+};
 
-  describe('initialize', function () {
+before(function (done) {
+  this.timeout(10000);
+  this.spy = sinon.spy();
+  analytics.ready(this.spy);
+  analytics.initialize({ HitTail: settings });
+  this.integration = analytics._integrations.HitTail;
+  this.options = this.integration.options;
+  when(function () { return window.htk; }, done);
+});
 
-    this.timeout(10000);
-
-    it('should call ready and load library', function (done) {
-      expect(window.htk).to.be(undefined);
-
-      var spy = sinon.spy();
-      analytics.ready(spy);
-      analytics.initialize({ 'HitTail' : test['HitTail'] });
-
-      // When the library loads `htk` is created.
-      var interval = setInterval(function () {
-        if (!window.htk) return;
-        expect(window.htk).not.to.be(undefined);
-        expect(spy.called).to.be(true);
-        clearInterval(interval);
-        done();
-      }, 20);
-    });
-
-    it('should store options', function () {
-      analytics.initialize({ 'HitTail' : test['HitTail'] });
-      expect(analytics._providers[0].options.siteId).to.equal(test['HitTail']);
-    });
-
+describe('#key', function () {
+  it('siteId', function () {
+    assert(this.integration.key == 'siteId');
   });
+});
+
+describe('#defaults', function () {
+  it('siteId', function () {
+    assert(this.integration.defaults.siteId === '');
+  });
+});
+
+describe('#initialize', function () {
+  it('should call ready', function () {
+    assert(this.spy.called);
+  });
+
+  it('should store options', function () {
+    assert(this.options.siteId == settings.siteId);
+  });
+});
 
 });

67d7b2e31ef0699c99fe78d75e887087c183dd16
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-09 13:06:38 -0700

    leadlander: cleanup

--- test/providers/leadlander.js
+++ test/providers/leadlander.js
@@ -1,30 +1,49 @@
 
 describe('LeadLander', function () {
 
-  var analytics = require('analytics')
-    , tick = require('next-tick');
+var analytics = window.analytics || require('analytics')
+  , assert = require('assert')
+  , sinon = require('sinon')
+  , when = require('when');
 
-  describe('initialize', function () {
+var settings = {
+  llactid: 'x'
+};
 
-    this.timeout(10000);
+before(function (done) {
+  this.timeout(10000);
+  this.spy = sinon.spy();
+  analytics.ready(this.spy);
+  analytics.initialize({ LeadLander: settings });
+  this.integration = analytics._integrations.LeadLander;
+  this.options = this.integration.options;
+  when(function () { return window.trackalyzer; }, done);
+});
 
-    it('should call ready and load library', function (done) {
-
-      var spy = sinon.spy();
-      analytics.ready(spy);
-      analytics.initialize({ 'LeadLander' : test['LeadLander'] });
-
-      tick(function () {
-        expect(spy.called).to.be(true);
-        done();
-      });
-    });
-
-    it('should store options', function () {
-      analytics.initialize({ 'LeadLander' : test['LeadLander'] });
-      expect(analytics._providers[0].options.llactid).to.equal(test['LeadLander']);
-    });
-
+describe('#key', function () {
+  it('llactid', function () {
+    assert(this.integration.key == 'llactid');
   });
+});
+
+describe('#defaults', function () {
+  it('llactid', function () {
+    assert(this.integration.defaults.llactid === null);
+  });
+});
+
+describe('#initialize', function () {
+  it('should call ready', function () {
+    assert(this.spy.called);
+  });
+
+  it('should store options', function () {
+    assert(this.options.llactid == settings.llactid);
+  });
+
+  it('should pass options to LeadLander', function () {
+    assert(window.llactid == settings.llactid);
+  });
+});
 
 });

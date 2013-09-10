c6e12d08b89c355908d9ac53ce1b764a653e69b5
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-10 14:51:32 -0700

    quantcast: cleanup

--- test/providers/quantcast.js
+++ test/providers/quantcast.js
@@ -1,40 +1,45 @@
+
 describe('Quantcast', function () {
 
-  var analytics = require('analytics');
+var analytics = window.analytics || require('analytics')
+  , assert = require('assert')
+  , sinon = require('sinon')
+  , when = require('when');
 
+var settings = {
+  pCode: 'x'
+};
 
-  describe('initialize', function () {
+before(function (done) {
+  this.timeout(10000);
+  this.spy = sinon.spy();
+  analytics.ready(this.spy);
+  analytics.initialize({ Quantcast: settings });
+  this.integration = analytics._integrations.Quantcast;
+  this.options = this.integration.options;
+  when(function () { return window.__qc; }, done);
+});
 
-    this.timeout(10000);
-
-    it('should call ready and load library', function (done) {
-      var spy  = sinon.spy()
-        , push = Array.prototype.push;
-
-      expect(window._qevents).to.be(undefined);
-
-      analytics.ready(spy);
-      analytics.initialize({ 'Quantcast' : test['Quantcast'] });
-
-      expect(window._qevents).not.to.be(undefined);
-      expect(window._qevents.push).to.equal(push);
-
-      // When the library loads, it will overwrite the push method.
-      var interval = setInterval(function () {
-        if (window._qevents.push === push) return;
-        expect(window._qevents.push).not.to.equal(push);
-        expect(window.__qc).not.to.be(undefined);
-        expect(spy.called).to.be(true);
-        clearInterval(interval);
-        done();
-      }, 20);
-    });
-
-    it('should store options', function () {
-      analytics.initialize({ 'Quantcast' : test['Quantcast'] });
-      expect(analytics._providers[0].options.pCode).to.equal(test['Quantcast']);
-    });
-
+describe('#key', function () {
+  it('pCode', function () {
+    assert(this.integration.key == 'pCode');
   });
+});
+
+describe('#defaults', function () {
+  it('pCode', function () {
+    assert(this.integration.defaults.pCode === null);
+  });
+});
+
+describe('#initialize', function () {
+  it('should call ready', function () {
+    assert(this.spy.called);
+  });
+
+  it('should store options', function () {
+    assert(this.options.pCode == settings.pCode);
+  });
+});
 
 });

7147cc912fc8c8dd13b9479fea725269afeb1372
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-10 20:31:51 -0700

    readd utils

---
+++ test/server/utils.js
@@ -0,0 +1,100 @@
+
+    window.test = {
+      userId : 'user',
+      traits : {
+        name    : 'Zeus',
+        email   : 'zeus@segment.io',
+        created : new Date()
+      },
+      groupId : 'group',
+      groupProperties : {
+        name      : 'Group',
+        employees : 42,
+        plan      : 'Gold'
+      },
+      event : 'event',
+      properties : {
+        type    : 'uncouth',
+        revenue : 29.99
+      },
+      context : {
+        providers : {
+          all : true
+        }
+      },
+      url : '/url',
+      newUserId : 'new',
+      oldUserId : 'old',
+      logMessage : 'something',
+      logError : new Error('something'),
+      logProperties : {
+        date : new Date()
+      },
+      'Amplitude' : '07808866adb2510adf19ee69e8fc2201',
+      'BugHerd' : '7917d741-16cc-4c2b-bb1a-bdd903d53d72',
+      'Chartbeat' : {
+        uid    : 'x',
+        domain : 'example.com'
+      },
+      'Clicky' : 'x',
+      'ClickTale' : {
+        projectId      : '19370',
+        recordingRatio : '0.0089',
+        partitionId    : 'www14'
+      },
+      'comScore' : {
+        c2 : 'x'
+      },
+      'CrazyEgg' : '00138301',
+      'Customer.io' : 'x',
+      'Errorception' : '506b76b52f52c3f662000140',
+      'FoxMetrics' : '5135085424023236bca9c08c',
+      'Gauges' : 'x',
+      'Get Satisfaction' : 5005,
+      'Google Analytics' : {
+        classic: {
+          trackingId : 'x'
+        },
+        universal: {
+          universalClient: true,
+          trackingId : 'x'
+        }
+      },
+      'GoSquared' : 'x',
+      'Heap' : 'x',
+      'HitTail' : 'x',
+      'HubSpot' : 'x',
+      'Improvely' : {
+        domain    : 'demo',
+        projectId : 1
+      },
+      'Intercom' : {
+        appId     : 'x',
+        activator : '#someElement',
+        counter   : true
+      },
+      'KISSmetrics' : '67f57ae9d61a6981fa07d141bec8c6c37e8b88c7',
+      'Klaviyo' : 'x',
+      'LeadLander' : 'x',
+      'LiveChat' : '1520', //'2328351',
+      'Mixpanel' : 'x',
+      'Olark' : '5798-949-10-1692',
+      'Optimizely' : true,
+      'Perfect Audience' : '4ff6ade4361ed500020000a5',
+      'Pingdom' : '5168f8c6abe53db732000000',
+      'Preact' : {
+        projectCode : 'x'
+      },
+      'Quantcast' : 'x',
+      'Qualaroo' : {
+        customerId : '47517',
+        siteToken  : '9Fd'
+      },
+      'Sentry' : 'x',
+      'SnapEngage' : '782b737e-487f-4117-8a2b-2beb32b600e5',
+      'USERcycle' : 'x',
+      'userfox' : '4v2erxr9c5vzqsy35z9gnk6az',
+      'Vero' : 'x',
+      'Visual Website Optimizer' : 44207,
+      'Woopra' : 'x'
+    };

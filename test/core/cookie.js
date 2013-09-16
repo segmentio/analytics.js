03412a6c08339918a864f719416c08bf52ca6902
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-16 15:23:54 -0700

    cleanup cookie tests

--- test/core/cookie.js
+++ test/core/cookie.js
@@ -1,58 +1,53 @@
 
 describe('Cookie', function () {
 
-var cookie = require('analytics/lib/cookie.js');
+var assert = require('assert')
+  , cookie = require('analytics/lib/cookie.js')
+  , equal = require('equals');
 
-after(function () {
+afterEach(function () {
   cookie.options({}); // reset to defaults
+  cookie.remove('x');
 });
 
-describe('#get()', function () {
+describe('#get', function () {
+
   it('should not not get an empty cookie', function () {
-    expect(cookie.get('abc')).to.be(null);
+    assert(cookie.get('abc') === null);
   });
 
   it('should get an existing cookie', function () {
     cookie.set('x', { a : 'b' });
-    expect(cookie.get('x')).to.eql({ a : 'b' });
-    cookie.remove('x');
+    assert(equal(cookie.get('x'), { a : 'b' }));
   });
 
   it('should not throw an error on a malformed cookie', function () {
     document.cookie="x=y";
-    expect(cookie.get('x')).to.be(null);
-    cookie.remove('x');
+    assert(cookie.get('x') === null);
   });
 });
 
-
-describe('#set()', function () {
-  it('should be able to set a cookie', function () {
+describe('#set', function () {
+  it('should set a cookie', function () {
     cookie.set('x', { a : 'b' });
-    expect(cookie.get('x')).to.eql({ a : 'b' });
-    cookie.remove('x');
+    assert(equal(cookie.get('x'), { a : 'b' }));
   });
 });
 
-
-describe('#remove()', function () {
-  it('should be able to remove a cookie', function () {
+describe('#remove', function () {
+  it('should remove a cookie', function () {
     cookie.set('x', { a : 'b' });
-    expect(cookie.get('x')).to.eql({ a : 'b' });
+    assert(equal(cookie.get('x'), { a : 'b' }));
     cookie.remove('x');
-    expect(cookie.get('x')).to.be(null);
+    assert(cookie.get('x') === null);
   });
 });
 
-
-describe('#options()', function () {
-  it('should be able to save options', function () {
-    cookie.options({
-      path: '/xyz'
-    });
-
-    expect(cookie.options().path).to.eql('/xyz');
-    expect(cookie.options().maxage).to.eql(31536000000);
+describe('#options', function () {
+  it('should save options', function () {
+    cookie.options({ path: '/xyz' });
+    assert(equal(cookie.options().path, '/xyz'));
+    assert(equal(cookie.options().maxage, 31536000000));
   });
 });
 

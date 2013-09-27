d3b5316778e755761e39ec8ed9bec7475ad48efc
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-26 20:47:10 -0700

    added new uservoice integration support

--- test/integrations/uservoice.js
+++ test/integrations/uservoice.js
@@ -3,15 +3,15 @@ describe('UserVoice', function () {
 
 var analytics = window.analytics || require('analytics')
   , assert = require('assert')
+  , jQuery = require('jquery')
   , sinon = require('sinon')
+  , unix = require('to-unix-timestamp')
   , when = require('when');
 
+describe('New', function () {
+
 var settings = {
-  widgetId: 'mhz5Op4MUft592O8Q82MwA',
-  forumId: 221539,
-  tabLabel: 'test',
-  defaultMode: 'feedback',
-  tabColor: '#ff0000'
+  apiKey: 'EvAljSeJvWrrIidgVvI2g'
 };
 
 before(function (done) {
@@ -31,18 +31,66 @@ describe('#name', function () {
 });
 
 describe('#key', function () {
-  it('widgetId', function () {
-    assert(this.integration.key == 'widgetId');
+  it('apiKey', function () {
+    assert(this.integration.key == 'apiKey');
   });
 });
 
 describe('#defaults', function () {
-  it('widgetId', function () {
-    assert(this.integration.defaults.widgetId === '');
+  it('apiKey', function () {
+    assert(this.integration.defaults.apiKey === '');
+  });
+
+  it('classic', function () {
+    assert(this.integration.defaults.classic === false);
+  });
+
+  it('widget', function () {
+    assert(this.integration.defaults.widget === true);
+  });
+
+  it('mode', function () {
+    assert(this.integration.defaults.mode === 'contact');
+  });
+
+  it('position', function () {
+    assert(this.integration.defaults.position === 'bottom-right');
+  });
+
+  it('color', function () {
+    assert(this.integration.defaults.color === '#448dd6');
+  });
+
+  it('trigger', function () {
+    assert(this.integration.defaults.trigger === null);
+  });
+
+  it('triggerColor', function () {
+    assert(this.integration.defaults.triggerColor === '#ffffff');
+  });
+
+  it('triggerBackgroundColor', function () {
+    assert(this.integration.defaults.triggerBackgroundColor === 'rgba(46, 49, 51, 0.6)');
   });
 
   it('forumId', function () {
     assert(this.integration.defaults.forumId === null);
+  });
+
+  it('linkColor', function () {
+    assert(this.integration.defaults.linkColor === '#007dbf');
+  });
+
+  it('defaultMode', function () {
+    assert(this.integration.defaults.defaultMode === 'support');
+  });
+
+  it('tabLabel', function () {
+    assert(this.integration.defaults.tabLabel === 'Feedback & Support');
+  });
+
+  it('tabInverted', function () {
+    assert(this.integration.defaults.tabInverted === false);
   });
 });
 
@@ -52,7 +100,108 @@ describe('#initialize', function () {
   });
 
   it('should store options', function () {
-    assert(this.options.widgetId == settings.widgetId);
+    assert(this.options.apiKey == settings.apiKey);
+  });
+
+  it('should show the trigger', function (done) {
+    when(function () { return jQuery('.uv-icon').length; }, done);
+  });
+});
+
+describe('#identify', function () {
+  beforeEach(function () {
+    analytics.user().reset();
+    this.stub = sinon.stub(window.UserVoice, 'push');
+  });
+
+  afterEach(function () {
+    this.stub.restore();
+  });
+
+  it('should send an id', function () {
+    analytics.identify('id');
+    assert(this.stub.calledWith(['identify', { id: 'id' }]));
+  });
+
+  it('should send traits', function () {
+    analytics.identify({ trait: true });
+    assert(this.stub.calledWith(['identify', { trait: true }]));
+  });
+
+  it('should send an id and traits', function () {
+    analytics.identify({ id: 'id', trait: true });
+    assert(this.stub.calledWith(['identify', { id: 'id', trait: true }]));
+  });
+
+  it('should convert a created date', function () {
+    var date = new Date();
+    analytics.identify({ created: date });
+    assert(this.stub.calledWith(['identify', { created_at: unix(date) }]));
+  });
+});
+
+describe('#group', function () {
+  beforeEach(function () {
+    analytics.group().reset();
+    this.stub = sinon.stub(window.UserVoice, 'push');
+  });
+
+  afterEach(function () {
+    this.stub.restore();
+  });
+
+  it('should send an id', function () {
+    analytics.group('id');
+    assert(this.stub.calledWith(['identify', { account: { id: 'id' }}]));
+  });
+
+  it('should send properties', function () {
+    analytics.group({ property: true });
+    assert(this.stub.calledWith(['identify', { account: { property: true }}]));
+  });
+
+  it('should send an id and properties', function () {
+    analytics.group({ id: 'id', property: true });
+    assert(this.stub.calledWith(['identify', { account: { id: 'id', property: true }}]));
+  });
+
+  it('should convert a created date', function () {
+    var date = new Date();
+    analytics.group({ created: date });
+    assert(this.stub.calledWith(['identify', { account: { created_at: unix(date) }}]));
+  });
+});
+
+});
+
+describe('Classic', function () {
+
+var settings = {
+  classic: true,
+  apiKey: 'mhz5Op4MUft592O8Q82MwA',
+  forumId: 221539,
+  tabLabel: 'test',
+  defaultMode: 'feedback',
+  triggerBackgroundColor: '#ff0000'
+};
+
+before(function (done) {
+  this.timeout(10000);
+  this.spy = sinon.spy();
+  analytics.ready(this.spy);
+  analytics.initialize({ UserVoice: settings });
+  this.integration = analytics._integrations.UserVoice;
+  this.options = this.integration.options;
+  when(function () { return window.UserVoice.account; }, done);
+});
+
+describe('#initialize', function () {
+  it('should call ready', function () {
+    assert(this.spy.called);
+  });
+
+  it('should store options', function () {
+    assert(this.options.apiKey == settings.apiKey);
     assert(this.options.forumId == settings.forumId);
   });
 
@@ -89,6 +238,8 @@ describe('#identify', function () {
     analytics.identify({ id: 'id', trait: true });
     assert(this.stub.calledWith(['setCustomFields', { id: 'id', trait: true }]));
   });
+});
+
 });
 
 });

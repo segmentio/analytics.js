cf26364dcd4ff06dce4f3314ba60c0f5db989e75
Calvin French-Owen <calvin@calv.info>
2013-10-24 16:45:26 -0700

    test.adroll: converting test to new format

--- test/integrations/adroll.js
+++ test/integrations/adroll.js
@@ -1,61 +1,84 @@
 
 describe('AdRoll', function () {
 
-var analytics = window.analytics || require('analytics')
-  , assert = require('assert')
-  , equal = require('equals')
-  , sinon = require('sinon')
-  , user = require('analytics/lib/user')
-  , when = require('when');
-
-var settings = {
-  advId: 'LYFRCUIPPZCCTOBGRH7G32',
-  pixId: 'V7TLXL5WWBA5NOU5MOJQW4'
-};
-
-before(function (done) {
-  user.identify('id', { trait: true });
+  var AdRoll = require('analytics/lib/integrations/adroll');
+  var assert = require('assert');
+  var equal = require('equals');
+  var sinon = require('sinon');
+  var user = require('analytics/lib/user');
+  var when = require('when');
+  var noop = function () {};
+  var adroll;
   this.timeout(10000);
-  this.spy = sinon.spy();
-  analytics.ready(this.spy);
-  analytics.initialize({ AdRoll: settings });
-  this.integration = analytics._integrations.AdRoll;
-  this.options = this.integration.options;
-  when(function () { return window.__adroll; }, done);
-});
 
-describe('#name', function () {
-  it('AdRoll', function () {
-    assert(this.integration.name == 'AdRoll');
-  });
-});
+  var settings = {
+    advId: 'LYFRCUIPPZCCTOBGRH7G32',
+    pixId: 'V7TLXL5WWBA5NOU5MOJQW4'
+  };
 
-describe('#defaults', function () {
-  it('advId', function () {
-    assert(this.integration.defaults.advId === '');
+  beforeEach(function () {
+    adroll = new AdRoll(settings);
   });
 
-  it('pixId', function () {
-    assert(this.integration.defaults.pixId === '');
-  });
-});
-
-describe('#initialize', function () {
-  it('should call ready', function () {
-    assert(this.spy.called);
+  describe('#name', function () {
+    it('AdRoll', function () {
+      assert(adroll.name == 'AdRoll');
+    });
   });
 
-  it('should store options', function () {
-    assert(this.options.advId == settings.advId);
-    assert(this.options.pixId == settings.pixId);
+  describe('#defaults', function () {
+    it('advId', function () {
+      assert(adroll.defaults.advId === '');
+    });
+
+    it('pixId', function () {
+      assert(adroll.defaults.pixId === '');
+    });
   });
 
-  it('should set custom data', function () {
-    assert(equal(window.adroll_custom_data, {
-      id: 'id',
-      trait: true
-    }));
-  });
-});
+  describe('#load', function () {
+    beforeEach(function () {
+      // AdRoll js requires these vars to be set for loading
+      window.adroll_adv_id = settings.advId;
+      window.adroll_pix_id = settings.pixId;
+    });
 
+    it('should load window.__adroll', function (done) {
+      adroll.load();
+      when(function () { return window.__adroll; }, done);
+    });
+
+    it('should callback', function (done) {
+      adroll.load(done);
+    });
+  });
+
+  describe('#initialize', function () {
+    beforeEach(function () {
+      adroll = new AdRoll(settings, noop);
+      delete window.adroll_adv_id;
+      delete window.adroll_pix_id;
+    });
+
+    it('should initialize the adroll variables', function () {
+      adroll.initialize();
+      assert(window.adroll_adv_id === settings.advId);
+      assert(window.adroll_pix_id === settings.pixId);
+    });
+
+    it('should call #load', function () {
+      var spy = sinon.spy(adroll, 'load');
+      adroll.initialize();
+      assert(spy.called);
+    });
+
+    it('should set custom data', function () {
+      user.identify('id', { trait: true });
+      adroll.initialize();
+      assert(equal(window.adroll_custom_data, {
+        id: 'id',
+        trait: true
+      }));
+    });
+  });
 });

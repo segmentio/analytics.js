af1b976da6aa3aa107f903f2bf22cc5315dbbf12
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-11-05 15:07:45 -0800

    crazyegg: convert tests

--- test/integrations/crazy-egg.js
+++ test/integrations/crazy-egg.js
@@ -1,45 +1,49 @@
 
 describe('Crazy Egg', function () {
-  this.timeout(10000);
 
+  var assert = require('assert');
+  var CrazyEgg = require('analytics/lib/integrations/crazy-egg');
+  var equal = require('equals');
+  var sinon = require('sinon');
+  var test = require('integration-tester');
+  var when = require('when');
+
+  var crazyegg;
   var settings = {
     accountNumber: '00138301'
   };
 
-  var assert = require('assert');
-  var CrazyEgg = require('analytics/lib/integrations/crazy-egg');
-  var crazyegg = new CrazyEgg(settings);
-  var equal = require('equals');
-  var sinon = require('sinon');
-  var when = require('when');
-
-  describe('#name', function () {
-    it('Crazy Egg', function () {
-      assert(crazyegg.name == 'Crazy Egg');
-    });
+  beforeEach(function () {
+    crazyegg = new CrazyEgg(settings);
+    crazyegg.initialize(); // noop
   });
 
-  describe('#defaults', function () {
-    it('accountNumber', function () {
-      assert(crazyegg.defaults.accountNumber === '');
-    });
+  afterEach(function () {
+    crazyegg.reset();
   });
 
-  describe('#exists', function () {
-    after(function () {
-      window.CE2 = undefined;
+  it('should have the right settings', function () {
+    test(crazyegg)
+      .name('Crazy Egg')
+      .assumesPageview()
+      .readyOnLoad()
+      .global('CE2')
+      .option('accountNumber', '');
+  });
+
+  describe('#initialize', function () {
+    beforeEach(function () {
+      crazyegg.load = sinon.spy(); // prevent loading
     });
 
-    it('should check for window.CE2', function () {
-      window.CE2 = {};
-      assert(crazyegg.exists());
-      window.CE2 = undefined;
-      assert(!crazyegg.exists());
+    it('should call #load', function () {
+      crazyegg.initialize();
+      assert(crazyegg.load.called);
     });
   });
 
   describe('#load', function () {
-    it('should create window.CE2', function (done) {
+    it('should create window.__adroll', function (done) {
       assert(!window.CE2);
       crazyegg.load();
       when(function () { return window.CE2; }, done);
@@ -47,24 +51,6 @@ describe('Crazy Egg', function () {
 
     it('should callback', function (done) {
       crazyegg.load(done);
-    });
-  });
-
-  describe('#initialize', function () {
-    var load;
-
-    beforeEach(function () {
-      window.CE2 = undefined;
-      load = sinon.spy(crazyegg, 'load');
-    });
-
-    afterEach(function () {
-      load.restore();
-    });
-
-    it('should call #load', function () {
-      crazyegg.initialize();
-      assert(load.called);
     });
   });
 

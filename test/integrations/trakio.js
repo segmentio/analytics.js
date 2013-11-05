f37b19f02c461bfc08c343b2b9387278766800a9
Calvin French-Owen <calvin@calv.info>
2013-11-05 12:30:59 -0800

    trak: updating integration and tests

--- test/integrations/trakio.js
+++ test/integrations/trakio.js
@@ -1,161 +1,165 @@
 
-describe('trak-io', function () {
+describe('trak.io', function () {
 
-var analytics = window.analytics || require('analytics')
-  , assert = require('assert')
-  , sinon = require('sinon')
-  , when = require('when');
+  var Trakio = require('analytics/lib/integrations/trakio');
+  var assert = require('assert');
+  var sinon = require('sinon');
+  var test = require('integration-tester');
+  var when = require('when');
 
-var settings = {
-  token: '740d36a79fb593bbc034a3ac934bc04f5a591c0c'
-};
+  var trakio;
+  var settings = {
+    token: '740d36a79fb593bbc034a3ac934bc04f5a591c0c'
+  };
 
-before(function (done) {
-  this.timeout(10000);
-  this.spy = sinon.spy();
-  analytics.ready(this.spy);
-  analytics.initialize({ 'trak.io': settings });
-  this.integration = analytics._integrations['trak.io'];
-  this.options = this.integration.options;
-  when(function () { return window.trak.io.loaded; }, done);
-});
-
-describe('#name', function () {
-  it('trak.io', function () {
-    assert(this.integration.name == 'trak.io');
-  });
-});
-
-describe('#key', function () {
-  it('token', function () {
-    assert(this.integration.key == 'token');
-  });
-});
-
-describe('#defaults', function () {
-  it('initialPageview', function () {
-    assert(this.integration.defaults.initialPageview === true);
-  });
-
-  it('pageview', function () {
-    assert(this.integration.defaults.pageview === true);
-  });
-
-  it('token', function () {
-    assert(this.integration.defaults.token === '');
-  });
-});
-
-describe('initialize', function () {
-  it('should call ready', function () {
-    assert(this.spy.called);
-  });
-
-  it('should store options with defaults', function () {
-    assert(this.options.initialPageview == this.integration.defaults.initialPageview);
-    assert(this.options.pageview == this.integration.defaults.pageview);
-    assert(this.options.token == settings.token);
-  });
-});
-
-describe('#identify', function () {
   beforeEach(function () {
-    analytics.user().reset();
-    this.spy = sinon.spy(window.trak.io, 'identify');
+    trakio = new Trakio(settings);
   });
 
   afterEach(function () {
-    this.spy.restore();
+    trakio.reset();
   });
 
-  it('should send id', function () {
-    analytics.identify('id');
-    assert(this.spy.calledWith('id'));
+  it('should store the right settings', function () {
+    test(trakio)
+      .name('trak.io')
+      .assumesPageview()
+      .readyOnInitialize()
+      .global('trak')
+      .option('token', '')
+      .option('trackNamedPages', true);
   });
 
-  it('should send traits', function () {
-    analytics.identify({ trait: true });
-    assert(this.spy.calledWith({ trait: true }));
-  });
 
-  it('should send an id and traits', function () {
-    analytics.identify('id', { trait: true });
-    assert(this.spy.calledWith('id', { trait: true }));
-  });
-
-  it('should alias traits', function () {
-    analytics.identify({
-      avatar: 'avatar',
-      firstName: 'first',
-      lastName: 'last'
+  describe('#load', function () {
+    it('should load the trak object', function (done) {
+      trakio.load();
+      when(function () {
+        return window.trak && window.trak.loaded;
+      }, done);
     });
-    assert(this.spy.calledWith({
-      avatar_url: 'avatar',
-      first_name: 'first',
-      last_name: 'last',
-      name: 'first last' // gets added automatically
-    }));
-  });
-});
-
-describe('#track', function () {
-  beforeEach(function () {
-    this.spy = sinon.spy(window.trak.io, 'track');
   });
 
-  afterEach(function () {
-    this.spy.restore();
+
+  describe('#initialize', function () {
+    it('should call load', function () {
+      trakio.load = sinon.spy();
+      trakio.initialize();
+      assert(trakio.load.called);
+    });
+
+    it('should set up the window.trak.io variables', function () {
+      trakio.initialize();
+      assert(window.trak.io);
+      assert(window.trak.io.identify);
+      assert(window.trak.io.track);
+      assert(window.trak.io.alias);
+    });
   });
 
-  it('should send an event', function () {
-    analytics.track('event');
-    assert(this.spy.calledWith('event'));
+  describe('#identify', function () {
+    beforeEach(function () {
+      trakio.initialize();
+      window.trak.io.identify = sinon.spy();
+    });
+
+    it('should send id', function () {
+      trakio.identify('id');
+      assert(window.trak.io.identify.calledWith('id', {}));
+    });
+
+    it('should send traits', function () {
+      trakio.identify(null, { trait: true });
+      assert(window.trak.io.identify.calledWith({ trait: true }));
+    });
+
+    it('should send an id and traits', function () {
+      trakio.identify('id', { trait: true });
+      assert(window.trak.io.identify.calledWith('id', { trait: true }));
+    });
+
+    it('should alias traits', function () {
+      trakio.identify(null, {
+        avatar: 'avatar',
+        firstName: 'first',
+        lastName: 'last'
+      });
+      assert(window.trak.io.identify.calledWith({
+        avatar_url: 'avatar',
+        first_name: 'first',
+        last_name: 'last'
+      }));
+    });
   });
 
-  it('should send an event and properties', function () {
-    analytics.track('event', { property: true });
-    assert(this.spy.calledWith('event', { property: true }));
-  });
-});
+  describe('#track', function () {
+    beforeEach(function () {
+      trakio.initialize();
+      window.trak.io.track = sinon.spy();
+    });
 
-describe('#pageview', function () {
-  beforeEach(function () {
-    this.spy = sinon.spy(window.trak.io, 'page_view');
-  });
+    it('should send an event', function () {
+      trakio.track('event');
+      assert(window.trak.io.track.calledWith('event'));
+    });
 
-  afterEach(function () {
-    this.spy.restore();
-  });
-
-  it('should call pageview', function () {
-    analytics.pageview();
-    assert(this.spy.called);
+    it('should send an event and properties', function () {
+      trakio.track('event', { property: true });
+      assert(window.trak.io.track.calledWith('event', { property: true }));
+    });
   });
 
-  it('should send a url', function () {
-    analytics.pageview('url');
-    assert(this.spy.calledWith('url'));
-  });
-});
+  describe('#page', function () {
+    beforeEach(function () {
+      trakio.initialize();
+      window.trak.io.page_view = sinon.spy();
+    });
 
-describe('#alias', function () {
-  beforeEach(function () {
-    this.spy = sinon.spy(window.trak.io, 'alias');
+    it('should call pageview', function () {
+      trakio.page();
+      assert(window.trak.io.page_view.called);
+    });
+
+    it('should send a page name', function () {
+      trakio.page('Signup');
+      assert(window.trak.io.page_view.calledWith(undefined, 'Signup'));
+    });
+
+    it('should send properties if they are included', function () {
+      var title = 'x';
+      var url = 'y';
+      trakio.page(null, {
+        url: url,
+        title: title
+      });
+      assert(window.trak.io.page_view.calledWith(url, title));
+    });
+
+    it('should call #track for named pages by default', function () {
+      window.trak.io.track = sinon.spy();
+      trakio.page('Signup');
+      assert(window.trak.io.track.calledWith('Viewed Signup Page'));
+    });
   });
 
-  afterEach(function () {
-    this.spy.restore();
-  });
+  describe('#alias', function () {
+    beforeEach(function (done) {
+      trakio.initialize();
+      // wait for trak to load fully so we can use distinct_id
+      when(function () { return window.trak.io.loaded; }, function () {
+        window.trak.io.alias = sinon.spy();
+        done();
+      });
+    });
 
-  it('should send a new id', function () {
-    analytics.alias('new');
-    assert(this.spy.calledWith('new'));
-  });
+    it('should send a new id', function () {
+      trakio.alias('new');
+      assert(window.trak.io.alias.calledWith('new'));
+    });
 
-  it('should send a new id and an original id', function () {
-    analytics.alias('another', 'original');
-    assert(this.spy.calledWith('original', 'another'));
+    it('should send a new id and an original id', function () {
+      trakio.alias('another', 'original');
+      assert(window.trak.io.alias.calledWith('original', 'another'));
+    });
   });
-});
-
 });

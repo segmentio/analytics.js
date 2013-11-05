c007357c9a6dee80b9e4073170165dfc271b7631
Calvin French-Owen <calvin@calv.info>
2013-11-05 10:30:22 -0800

    uservoice: fixing tests and cloned alias, date conversion

--- test/integrations/uservoice.js
+++ test/integrations/uservoice.js
@@ -3,10 +3,14 @@ describe('UserVoice', function () {
 
   var UserVoice = require('analytics/lib/integrations/uservoice');
   var assert = require('assert');
+  var extend = require('extend');
+  var equal = require('equals');
+  var group = require('analytics/lib/group');
   var jQuery = require('jquery');
   var sinon = require('sinon');
   var test = require('integration-tester');
   var unix = require('to-unix-timestamp');
+  var user = require('analytics/lib/user');
   var when = require('when');
 
   var uservoice;
@@ -16,14 +20,17 @@ describe('UserVoice', function () {
 
   beforeEach(function () {
     uservoice = new UserVoice(settings);
+    user.reset();
+    group.reset();
   });
 
   afterEach(function () {
-    uservoice.restore();
+    uservoice.reset();
   });
 
   it('should store the right settings', function () {
-    test('UserVoice')
+    test(uservoice)
+      .name('UserVoice')
       .assumesPageview()
       .readyOnInitialize()
       .global('UserVoice')
@@ -48,173 +55,248 @@ describe('UserVoice', function () {
       .option('tabInverted', false);
   });
 
-  describe('#initialize', function () {
-    beforeEach(function () {
-      uservoice.load = sinon.spy();
+  describe('Non-Classic', function () {
+    describe('#initialize', function () {
+      var options = {
+        accent_color: '#448dd6',
+        apiKey: 'EvAljSeJvWrrIidgVvI2g',
+        classic: false,
+        classicMode: 'full',
+        defaultMode: 'support',
+        forum_id: null,
+        linkColor: '#007dbf',
+        mode: 'contact',
+        primaryColor: '#cc6d00',
+        showWidget: true,
+        tabColor: '#cc6d00',
+        tabInverted: false,
+        tabLabel: 'Feedback & Support',
+        tabPosition: 'middle-right',
+        trigger: null,
+        trigger_background_color: 'rgba(46, 49, 51, 0.6)',
+        trigger_color: '#ffffff',
+        trigger_position: 'bottom-right'
+      };
+
+      it('should show the trigger', function (done) {
+        uservoice.initialize();
+        when(function () { return jQuery('.uv-icon').length; }, done);
+      });
+
+      it('should call load', function () {
+        uservoice.load = sinon.spy(uservoice, 'load');
+        uservoice.initialize();
+        assert(uservoice.load.called);
+      });
+
+      it('should push the right options onto window.UserVoice', function () {
+        uservoice.initialize();
+        assert(equal(window.UserVoice[0], ['set', options]));
+      });
+
+      it('should push the autoprompt options', function () {
+        uservoice.initialize();
+        assert(equal(window.UserVoice[1], ['autoprompt', {}]));
+      });
+
+      it('should push the addTrigger options', function () {
+        uservoice.initialize();
+        assert(equal(window.UserVoice[2], ['addTrigger', options]));
+      });
     });
 
-    it('should call load', function () {
-      uservoice.initialize();
-      assert(uservoice.load.called);
+    describe('#identify', function () {
+      beforeEach(function () {
+        uservoice.initialize();
+        window.UserVoice.push = sinon.stub(window.UserVoice, 'push');
+      });
+
+      afterEach(function () {
+        window.UserVoice.push.restore();
+      });
+
+      it('should send an id', function () {
+        uservoice.identify('id');
+        assert(window.UserVoice.push.calledWith([
+          'identify',
+          { id: 'id' }
+        ]));
+      });
+
+      it('should send traits', function () {
+        uservoice.identify(null, { trait: true });
+        assert(window.UserVoice.push.calledWith([
+          'identify',
+          { trait: true }
+        ]));
+      });
+
+      it('should send an id and traits', function () {
+        uservoice.identify(null, { id: 'id', trait: true });
+        assert(window.UserVoice.push.calledWith([
+          'identify',
+          { id: 'id', trait: true }
+        ]));
+      });
+
+      it('should convert a created date', function () {
+        var date = new Date();
+        uservoice.identify(null, { created: date });
+        assert(window.UserVoice.push.calledWith([
+          'identify',
+          { created_at: unix(date) }
+        ]));
+      });
     });
 
-    it('should use identifyClassic if set to classic', function () {
+    describe('#group', function () {
+      beforeEach(function () {
+        uservoice.initialize();
+        window.UserVoice.push = sinon.stub(window.UserVoice, 'push');
+      });
+
+      afterEach(function () {
+        window.UserVoice.push.restore();
+      });
+
+      it('should send an id', function () {
+        uservoice.group('id');
+        assert(window.UserVoice.push.calledWith([
+          'identify',
+          { account: { id: 'id' }}
+        ]));
+      });
+
+      it('should send properties', function () {
+        uservoice.group(null, { property: true });
+        assert(window.UserVoice.push.calledWith([
+          'identify',
+          { account: { property: true }}
+        ]));
+      });
+
+      it('should send an id and properties', function () {
+        uservoice.group(null, { id: 'id', property: true });
+        assert(window.UserVoice.push.calledWith([
+          'identify',
+          { account: { id: 'id', property: true }}
+        ]));
+      });
+
+      it('should convert a created date', function () {
+        var date = new Date();
+        uservoice.group(null, { created: date });
+        assert(window.UserVoice.push.calledWith([
+          'identify',
+          { account: { created_at: unix(date) }}
+        ]));
+      });
+    });
+  });
+
+
+  describe('Classic', function () {
+    before(function () {
+      settings = {
+        classic: true,
+        classicMode: 'full',
+        apiKey: 'mhz5Op4MUft592O8Q82MwA',
+        forumId: 221539,
+        tabLabel: 'test',
+        defaultMode: 'feedback',
+        primaryColor: '#ffffff',
+      };
     });
 
-    it('should not have a group method if set to classic', function () {
+    describe('#initialize', function () {
+      beforeEach(function () {
+        uservoice.load = sinon.spy();
+        delete window.showClassicWidget;
+      });
+
+      it('should push the options onto window.UserVoice.push', function () {
+        uservoice.initialize();
+        assert(equal(window.UserVoice[0], [
+          'showTab',
+          'classic_widget',
+          {
+            accentColor: '#448dd6',
+            apiKey: 'mhz5Op4MUft592O8Q82MwA',
+            classic: true,
+            default_mode: 'feedback',
+            forum_id: 221539,
+            link_color: '#007dbf',
+            mode: 'full',
+            primary_color: '#ffffff',
+            showWidget: true,
+            tab_color: '#cc6d00',
+            tab_inverted: false,
+            tab_label: 'test',
+            tab_position: 'middle-right',
+            trigger: null,
+            triggerBackgroundColor: 'rgba(46, 49, 51, 0.6)',
+            triggerColor: '#ffffff',
+            triggerPosition: 'bottom-right'
+          }
+        ]));
+      });
+
+      it('should create the window.showClassicWidget function', function () {
+        assert(!window.showClassicWidget);
+        uservoice.initialize();
+        assert(window.showClassicWidget);
+      });
+
+      it('should call load', function () {
+        uservoice.initialize();
+        assert(uservoice.load.called);
+      });
+
+      it('should not have a group method if set to classic', function () {
+        uservoice.initialize();
+        assert(uservoice.group === undefined);
+      });
+
+      it('should show the tab', function (done) {
+        uservoice.initialize();
+        when(function () { return document.getElementById('uvTab'); }, done);
+      });
     });
 
-    it('should push the setup arguments onto the window.UserVoice object', function () {
 
+    describe('#identify', function () {
+      beforeEach(function () {
+        uservoice.initialize();
+        window.UserVoice.push = sinon.stub(window.UserVoice, 'push');
+      });
+
+      afterEach(function () {
+        window.UserVoice.push.restore();
+      });
+
+      it('should send an id', function () {
+        uservoice.identify('id');
+        assert(window.UserVoice.push.calledWith([
+          'setCustomFields',
+          { id: 'id' }
+        ]));
+      });
+
+      it('should send traits', function () {
+        uservoice.identify(null, { trait: true });
+        assert(window.UserVoice.push.calledWith([
+          'setCustomFields',
+          { trait: true }
+        ]));
+      });
+
+      it('should send an id and traits', function () {
+        uservoice.identify(null, { id: 'id', trait: true });
+        assert(window.UserVoice.push.calledWith([
+          'setCustomFields',
+          { id: 'id', trait: true }
+        ]));
+      });
     });
-
-
-    it('should call ready', function () {
-      assert(this.spy.called);
-    });
-
-    it('should store options', function () {
-      assert(this.options.apiKey == settings.apiKey);
-    });
-
-    it('should show the trigger', function (done) {
-      when(function () { return jQuery('.uv-icon').length; }, done);
-    });
   });
-
-describe('#identify', function () {
-  beforeEach(function () {
-    analytics.user().reset();
-    this.stub = sinon.stub(window.UserVoice, 'push');
-  });
-
-  afterEach(function () {
-    this.stub.restore();
-  });
-
-  it('should send an id', function () {
-    analytics.identify('id');
-    assert(this.stub.calledWith(['identify', { id: 'id' }]));
-  });
-
-  it('should send traits', function () {
-    analytics.identify({ trait: true });
-    assert(this.stub.calledWith(['identify', { trait: true }]));
-  });
-
-  it('should send an id and traits', function () {
-    analytics.identify({ id: 'id', trait: true });
-    assert(this.stub.calledWith(['identify', { id: 'id', trait: true }]));
-  });
-
-  it('should convert a created date', function () {
-    var date = new Date();
-    analytics.identify({ created: date });
-    assert(this.stub.calledWith(['identify', { created_at: unix(date) }]));
-  });
-});
-
-describe('#group', function () {
-  beforeEach(function () {
-    analytics.group().reset();
-    this.stub = sinon.stub(window.UserVoice, 'push');
-  });
-
-  afterEach(function () {
-    this.stub.restore();
-  });
-
-  it('should send an id', function () {
-    analytics.group('id');
-    assert(this.stub.calledWith(['identify', { account: { id: 'id' }}]));
-  });
-
-  it('should send properties', function () {
-    analytics.group({ property: true });
-    assert(this.stub.calledWith(['identify', { account: { property: true }}]));
-  });
-
-  it('should send an id and properties', function () {
-    analytics.group({ id: 'id', property: true });
-    assert(this.stub.calledWith(['identify', { account: { id: 'id', property: true }}]));
-  });
-
-  it('should convert a created date', function () {
-    var date = new Date();
-    analytics.group({ created: date });
-    assert(this.stub.calledWith(['identify', { account: { created_at: unix(date) }}]));
-  });
-});
-
-});
-
-describe('Classic', function () {
-
-var settings = {
-  classic: true,
-  classicMode: 'full',
-  apiKey: 'mhz5Op4MUft592O8Q82MwA',
-  forumId: 221539,
-  tabLabel: 'test',
-  defaultMode: 'feedback',
-  triggerBackgroundColor: '#ff0000'
-};
-
-before(function (done) {
-  this.timeout(10000);
-  this.spy = sinon.spy();
-  analytics.ready(this.spy);
-  analytics.initialize({ UserVoice: settings });
-  this.integration = analytics._integrations.UserVoice;
-  this.options = this.integration.options;
-  when(function () { return window.UserVoice.account; }, done);
-});
-
-describe('#initialize', function () {
-  it('should call ready', function () {
-    assert(this.spy.called);
-  });
-
-  it('should store options', function () {
-    assert(this.options.apiKey == settings.apiKey);
-    assert(this.options.forumId == settings.forumId);
-  });
-
-  it('should show the tab', function (done) {
-    when(function () { return document.getElementById('uvTab'); }, done);
-  });
-
-  it('should expose a showClassicWidget global', function () {
-    assert(window.showClassicWidget);
-  });
-});
-
-describe('#identify', function () {
-  beforeEach(function () {
-    analytics.user().reset();
-    this.stub = sinon.stub(window.UserVoice, 'push');
-  });
-
-  afterEach(function () {
-    this.stub.restore();
-  });
-
-  it('should send an id', function () {
-    analytics.identify('id');
-    assert(this.stub.calledWith(['setCustomFields', { id: 'id' }]));
-  });
-
-  it('should send traits', function () {
-    analytics.identify({ trait: true });
-    assert(this.stub.calledWith(['setCustomFields', { trait: true }]));
-  });
-
-  it('should send an id and traits', function () {
-    analytics.identify({ id: 'id', trait: true });
-    assert(this.stub.calledWith(['setCustomFields', { id: 'id', trait: true }]));
-  });
-});
-
-});
-
 });

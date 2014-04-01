2330232f4b506a7bbcaa658a91e68d32015e2c60
Amir Abu Shareb <yields@icloud.com>
2014-04-01 04:25:22 +0300

    upgrade gravy to 0.2.0

--- analytics.js
+++ analytics.js
@@ -1167,13 +1167,8 @@ function isEmpty (val) {
 });
 require.register("ianstormtaylor-is/index.js", function(exports, require, module){
 
-var isEmpty = require('is-empty');
-
-try {
-  var typeOf = require('type');
-} catch (e) {
-  var typeOf = require('component-type');
-}
+var isEmpty = require('is-empty')
+  , typeOf = require('type');
 
 
 /**

580875b5c6fc812993b46d42d9fc7d1e83b98c12
Amir Abu Shareb <yields@icloud.com>
2014-04-11 12:58:01 +0300

    Release 1.3.22

--- analytics.js
+++ analytics.js
@@ -1010,8 +1010,13 @@ module.exports = function (obj) {
 });
 require.register("ianstormtaylor-bind/index.js", function(exports, require, module){
 
-var bind = require('bind')
-  , bindAll = require('bind-all');
+try {
+  var bind = require('bind');
+} catch (e) {
+  var bind = require('bind-component');
+}
+
+var bindAll = require('bind-all');
 
 
 /**
@@ -13472,7 +13477,7 @@ analytics.require = require;
  * Expose `VERSION`.
  */
 
-exports.VERSION = '1.3.21';
+exports.VERSION = '1.3.22';
 
 
 /**

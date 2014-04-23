7829640745c69cf71c75314a2bf79af0d86e1a86
Amir Abu Shareb <yields@icloud.com>
2014-04-24 02:29:49 +0300

    upgrade integrations to 0.5.9

--- analytics.js
+++ analytics.js
@@ -5049,6 +5049,232 @@ function ecommerce(event, track, arr){
 
 
 });
+require.register("segmentio-analytics.js-integrations/lib/frontleaf.js", function(exports, require, module){
+
+var callback = require('callback');
+var integration = require('integration');
+var is = require('is');
+var load = require('load-script');
+
+/**
+ * Expose plugin.
+ */
+
+module.exports = exports = function (analytics) {
+  analytics.addIntegration(Frontleaf);
+};
+
+
+/**
+ * Expose `Frontleaf` integration.
+ */
+
+var Frontleaf = exports.Integration = integration('Frontleaf')
+  .assumesPageview()
+  .readyOnInitialize()
+  .global('_fl')
+  .global('_flBaseUrl')
+  .option('baseUrl', 'https://api.frontleaf.com')
+  .option('token', '')
+  .option('stream', '');
+
+
+/**
+ * Initialize.
+ *
+ * http://docs.frontleaf.com/#/technical-implementation/tracking-customers/tracking-beacon
+ *
+ * @param {Object} page
+ */
+
+Frontleaf.prototype.initialize = function (page) {
+  window._fl = window._fl || [];
+  window._flBaseUrl = window._flBaseUrl || this.options.baseUrl;
+
+  this._push('setApiToken', this.options.token);
+  this._push('setStream', this.options.stream);
+
+  this.load();
+};
+
+
+/**
+ * Loaded?
+ *
+ * @return {Boolean}
+ */
+
+Frontleaf.prototype.loaded = function () {
+  return is.array(window._fl) && window._fl.ready === true ;
+};
+
+
+/**
+ * Load.
+ *
+ * @param {Function} fn
+ */
+
+Frontleaf.prototype.load = function (fn) {
+  if (document.getElementById('_fl')) return callback.async(fn);
+
+  var script = load(window._flBaseUrl + '/lib/tracker.js', fn);
+  script.id = '_fl';
+};
+
+
+/**
+ * Identify.
+ *
+ * @param {Identify} identify
+ */
+
+Frontleaf.prototype.identify = function (identify) {
+  var userId = identify.userId();
+  if (userId) {
+    this._push('setUser', {
+      id : userId,
+      name : identify.name() || identify.username(),
+      data : clean(identify.traits())
+    });
+  }
+};
+
+
+/**
+ * Group.
+ *
+ * @param {Group} group
+ */
+
+Frontleaf.prototype.group = function (group) {
+  var groupId = group.groupId();
+  if (groupId) {
+    this._push('setAccount', {
+      id : groupId,
+      name : group.proxy('traits.name'),
+      data : clean(group.traits())
+    });
+  }
+};
+
+
+/**
+ * Track.
+ *
+ * @param {Track} track
+ */
+
+Frontleaf.prototype.track = function (track) {
+  var event = track.event();
+  if (event) {
+    this._push('event', event, clean(track.properties()));
+  }
+};
+
+
+/**
+ * Push a command onto the global Frontleaf queue.
+ *
+ * @param {String} command
+ * @return {Object} args
+ * @api private
+ */
+
+Frontleaf.prototype._push = function (command) {
+  var args = [].slice.call(arguments, 1);
+  window._fl.push(function(t) { t[command].apply(command, args); });
+}
+
+
+/**
+ * Clean all nested objects and arrays.
+ *
+ * @param {Object} obj
+ * @return {Object}
+ * @api private
+ */
+
+function clean(obj) {
+  var ret = {};
+
+  // Remove traits/properties that are already represented
+  // outside of the data container
+  var excludeKeys = ["id","name","firstName","lastName"];
+  var len = excludeKeys.length;
+  for (var i = 0; i < len; i++) {
+    clear(obj, excludeKeys[i]);
+  }
+
+  // Flatten nested hierarchy, preserving arrays
+  obj = flatten(obj);
+
+  // Discard nulls, represent arrays as comma-separated strings
+  for (var key in obj) {
+    var val = obj[key];
+    if (null == val) {
+      continue;
+    }
+
+    if (is.array(val)) {
+      ret[key] = val.toString();
+      continue;
+    }
+
+    ret[key] = val;
+  }
+
+  return ret;
+}
+
+/**
+ * Remove a property from an object if set.
+ *
+ * @param {Object} obj
+ * @param {String} key
+ * @api private
+ */
+
+function clear(obj, key) {
+  if (obj.hasOwnProperty(key)) {
+    delete obj[key];
+  }
+}
+
+
+/**
+ * Flatten a nested object into a single level space-delimited
+ * hierarchy.
+ *
+ * Based on https://github.com/hughsk/flat
+ *
+ * @param {Object} source
+ * @return {Object}
+ * @api private
+ */
+
+function flatten(source) {
+  var output = {};
+
+  function step(object, prev) {
+    for (var key in object) {
+      var value = object[key];
+      var newKey = prev ? prev + ' ' + key : key;
+
+      if (!is.array(value) && is.object(value)) {
+        return step(value, newKey);
+      }
+
+      output[newKey] = value;
+    }
+  }
+
+  step(source);
+
+  return output;
+}
+
+});
 require.register("segmentio-analytics.js-integrations/lib/gauges.js", function(exports, require, module){
 
 var callback = require('callback');
@@ -6589,7 +6815,7 @@ Intercom.prototype.group = function (group) {
  */
 
 Intercom.prototype.track = function(track){
-  window.Intercom('trackUserEvent', track.event(), track.traits());
+  window.Intercom('trackEvent', track.event(), track.properties());
 };
 
 /**
@@ -7279,6 +7505,7 @@ var each = require('each');
 var integration = require('integration');
 var load = require('load-script');
 var clone = require('clone');
+var when = require('when');
 
 
 /**
@@ -7298,6 +7525,9 @@ var LiveChat = exports.Integration = integration('LiveChat')
   .assumesPageview()
   .readyOnLoad()
   .global('__lc')
+  .global('__lc_inited')
+  .global('LC_API')
+  .global('LC_Invite')
   .option('group', 0)
   .option('license', '');
 
@@ -7312,7 +7542,6 @@ var LiveChat = exports.Integration = integration('LiveChat')
 
 LiveChat.prototype.initialize = function (page) {
   window.__lc = clone(this.options);
-  this.isLoaded = false;
   this.load();
 };
 
@@ -7324,7 +7553,7 @@ LiveChat.prototype.initialize = function (page) {
  */
 
 LiveChat.prototype.loaded = function () {
-  return this.isLoaded;
+  return !!(window.LC_API && window.LC_Invite);
 };
 
 
@@ -7338,8 +7567,9 @@ LiveChat.prototype.load = function (callback) {
   var self = this;
   load('//cdn.livechatinc.com/tracking.js', function(err){
     if (err) return callback(err);
-    self.isLoaded = true;
-    callback();
+    when(function(){
+      return self.loaded();
+    }, callback);
   });
 };
 
@@ -14795,6 +15025,7 @@ module.exports = [
   "evergage",
   "facebook-ads",
   "foxmetrics",
+  "frontleaf",
   "gauges",
   "get-satisfaction",
   "google-analytics",
@@ -15013,6 +15244,7 @@ require.alias("segmentio-analytics.js-integrations/lib/errorception.js", "analyt
 require.alias("segmentio-analytics.js-integrations/lib/evergage.js", "analytics/deps/integrations/lib/evergage.js");
 require.alias("segmentio-analytics.js-integrations/lib/facebook-ads.js", "analytics/deps/integrations/lib/facebook-ads.js");
 require.alias("segmentio-analytics.js-integrations/lib/foxmetrics.js", "analytics/deps/integrations/lib/foxmetrics.js");
+require.alias("segmentio-analytics.js-integrations/lib/frontleaf.js", "analytics/deps/integrations/lib/frontleaf.js");
 require.alias("segmentio-analytics.js-integrations/lib/gauges.js", "analytics/deps/integrations/lib/gauges.js");
 require.alias("segmentio-analytics.js-integrations/lib/get-satisfaction.js", "analytics/deps/integrations/lib/get-satisfaction.js");
 require.alias("segmentio-analytics.js-integrations/lib/google-analytics.js", "analytics/deps/integrations/lib/google-analytics.js");

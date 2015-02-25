7eb2a6c3305a45f1172f0916be60bca70877e46c
Nathan Houle <nathan@nathanhoule.com>
2015-02-25 22:24:48 +0000

    Updating analytics.js-integrations

--- analytics.js
+++ analytics.js
@@ -19155,7 +19155,7 @@ module.exports.User = User;
 5: [function(require, module, exports) {
 module.exports = {
   "name": "analytics",
-  "version": "2.6.8",
+  "version": "2.6.9",
   "main": "analytics.js",
   "dependencies": {},
   "devDependencies": {}

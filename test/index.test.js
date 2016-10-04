efd5cba32194e2deba1879239228619db653a232
Alois Barreras <alois@vicino.io>
2016-10-03 20:51:32 -0400

    added test to catch some errors

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,7 @@
 var assert = require('chai').assert;
 var integrations = require('../lib/integrations');
 var intersection = require('lodash.intersection');
+var analytics = require('../lib');
 
 describe('analytics.js', function () {
   var expectedIntegrations = [
@@ -47,5 +48,25 @@ describe('analytics.js', function () {
     var ints = Object.keys(integrations);
     var length = intersection(expectedIntegrations, ints).length;
     assert.equal(length, expectedIntegrations.length);
+  });
+
+  it('should initialize', function () {
+      var queue = window.analytics || [];
+      var snippetVersion = queue && queue.SNIPPET_VERSION ? parseFloat(queue.SNIPPET_VERSION, 10) : 0;
+
+      analytics.initialize({}, {
+          initialPageview: 0 === snippetVersion,
+          plan: {
+              track: {}
+          }
+      });
+
+      for (; queue && queue.length > 0;) {
+          var args = queue.shift();
+          var method = args.shift();
+          analytics[method] && analytics[method].apply(analytics, args)
+      }
+
+      window.analytics = analytics;
   });
 });

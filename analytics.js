b320f5e15154e59770a3644be5af98bf3949042c
Anthony Morris <anthony.morris@growthoptimized.com>
2017-04-28 10:34:21 -0700

    Update jQuery reference for chekcking middle mouse button

--- analytics.js
+++ analytics.js
@@ -5560,7 +5560,7 @@ module.exports = function isMeta (e) {
     if (e.metaKey || e.altKey || e.ctrlKey || e.shiftKey) return true;
 
     // Logic that handles checks for the middle mouse button, based
-    // on [jQuery](https://github.com/jquery/jquery/blob/master/src/event.js#L466).
+    // on [jQuery](https://github.com/jquery/jquery/blob/master/src/event.js#L634).
     var which = e.which, button = e.button;
     if (!which && button !== undefined) {
       return (!button & 1) && (!button & 2) && (button & 4);

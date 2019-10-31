8ca1b9f25fa516f61102f84901ff6136da04f9ed
Nikolaos Veneti <nikolaos.veneti@dreamdata.io>
2019-10-31 10:19:04 +0100

    build dev pointing to local enviroment

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,24 +1,34 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: path.join(__dirname, "/lib"),
-  resolve: {
-    extensions: [".js"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
-  output: {
-    library: "analytics",
-    libraryTarget: "umd",
-    filename: "analytics.js"
-  }
+    mode: 'development',
+    entry: path.join(__dirname, '/lib'),
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'string-replace-loader',
+                options: {
+                    multiple: [
+                        { search: 'api.dreamdata.cloud/v1', replace: 'localhost:8080/v1' },
+                        { search: 'https://', replace: 'http://' }
+                    ]
+                }
+            }
+        ]
+    },
+    output: {
+        library: 'analytics',
+        libraryTarget: 'umd',
+        filename: 'analytics.js'
+    }
 };
